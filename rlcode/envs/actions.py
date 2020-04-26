eabae7287d1eabca27461c6d1b752e1903e8b585
Kam, Dongki <dkkahm@gmail.com>
2020-04-26 18:42:49 +0900

    Environment5x5Moving

--- rlcode/envs/actions.py
+++ rlcode/envs/actions.py
@@ -2,15 +2,19 @@ LEFT = 0
 DOWN = 1
 RIGHT = 2
 UP = 3
+NONE = 4
+
 
 def action_name(a):
-  if a == LEFT:
-    return "LEFT"
-  elif a == DOWN:
-    return "DOWN"
-  elif a == RIGHT:
-    return "RIGHT"
-  elif a == UP:
-    return "UP"
-  else:
-    raise ValueError
+    if a == LEFT:
+        return "LEFT"
+    elif a == DOWN:
+        return "DOWN"
+    elif a == RIGHT:
+        return "RIGHT"
+    elif a == UP:
+        return "UP"
+    elif a == NONE:
+        return "NONE"
+    else:
+        raise ValueError
